84dba03493e4e7b3166c891afe8e706c7d22f8e0
Crystal Collins <CrystalKushwaha@gmail.com>
2023-02-20 23:33:15 -0600

    Added visual details

--- visuals.py
+++ visuals.py
@@ -1,3 +1,24 @@
 # This file defines constants that are used throughout the program,
 # such as the size of the window or the location of asset files.
+
+# Margins
+MARGIN_LEFT = 230
+MARGIN_TOP = 150
+
+# WINDOW SIZE
+WIDTH = 800
+HEIGHT = 600
+
+# COLORS
+black = (0, 0, 0)
+white = (255, 255, 255)
+cyan = (0, 255, 255)
+magenta = (255, 0, 255)
+cadet_blue = (152, 245, 255)
+coral = (255, 114, 86)
+orchid = (154, 50, 205)
+berry = (139, 58, 98)
+deep_sea = (105, 139, 105)
+grey = (59, 59, 59)
+
 pass
